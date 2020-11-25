f1a8fd8191351971a48133980fd64353ded546a4
o-henry <cksgod16@gmail.com>
2020-11-26 01:04:22 +0900

    Add Base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let [currentUser, setCurrentUser] = React.useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <div style={{ backgroundColor: "lightgray" }}>
+        <Header />
+      </div>
+
+      <div style={{ flex: 1 }}>
+        {currentUser ? (
+          <Dashboard user={currentUser} />
+        ) : (
+          ""
+          <LoginScreen onLogin={() => setCurrentUser({ name: "Michael" })} />
+        )}
+      </div>
+
+      <div style={{ backgroundColor: "lightgray" }}>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Headr</h1>
+    </div>
+  );
+}
+
+function LoginScreen({ onLogin }: any) {
+  return (
+    <div>
+      <h3>Please Login</h3>
+      <button onClick={onLogin}>Login</button>
+    </div>
+  );
+}
+function Dashboard({ user }: any) {
+  return (
+    <div>
+      <h2>The Dashboard</h2>
+      <DashboardNav />
+      <DashboardContent user={user} />
+    </div>
+  );
+}
+
+function DashboardNav() {
+  return (
+    <div>
+      <h3>Dashboard Nav</h3>
+    </div>
+  );
+}
+
+function DashboardContent({ user }: any) {
+  return (
+    <div>
+      <h3>Dashboard Content</h3>
+      <WelcomeMessage user={user} />
+    </div>
+  );
+}
+
+function WelcomeMessage({ user }: any) {
+  return (
+    <div>
+      <p>Welcome {user.name}!</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div>
+      <h1>Footer</h1>
     </div>
   );
 }
